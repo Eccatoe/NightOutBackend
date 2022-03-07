23672f2cc5532f4a79ba1c3d1f5d6347321d7b46
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-07 16:49:18 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,39 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+#Restaurants
+20.times do
+    Restaurant.create(
+        restaurant_name: Faker::Restaurant.name, 
+        cuisine: Faker::Restaurant.type, 
+        description: Faker::Restaurant.description,
+        address: Faker::Address.street_address,
+        price: ["$", "$$", "$$$"].sample,
+        avg_turn_time: rand(90...120)
+    )
+end
+
+#Reservations
+20.times do
+    details: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+    restaurant_id: rand(1..Restaurant.all.length),
+    rideshare_id: rand(1..Rideshare.all.length),
+    user_id: rand(1..User.all.length),
+    num_guests: rand(1..6)
+end
+
+#Rideshares
+Rideshare.create(company: "Lyft")
+Rideshare.create(company: "Uber")
+Rideshare.create(company: "Car Service")
+
+#Users
+20.times do
+    User.create(
+        name: Faker::Name.name,
+        cc_info: Faker::Number.number(digits: 16),
+        address: Faker::Address.street_address
+    )
+end
+
 
 puts "✅ Done seeding!"
