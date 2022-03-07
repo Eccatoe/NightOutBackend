083ba593a7dd8b62a2e13c644b6395761e3b261e
Marc Cavazos <marcavazos19@icloud.com>
2022-03-07 11:46:59 -0600

    restaurant model, migration

--- db/migrate/20220307173722_create_restaurants.rb
+++ db/migrate/20220307173722_create_restaurants.rb
@@ -1,4 +1,11 @@
 class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
+    create_table :restaurants do |t|
+      t.string :restaurant_name
+      t.string :address
+      t.string :price
+      t.integer :avg_turn_time
+      t.string :cuisine
+      t.string :description
   end
 end
