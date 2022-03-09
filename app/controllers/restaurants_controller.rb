4e9b88186fe086297ab3ae0c46edc624407eb526
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-09 11:10:37 -0500

    switching

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,10 +6,11 @@ class RestaurantsController < ApplicationController
 
 
     get'/restaurants/:cuisine' do
-        Restaurant.all.find_by(cuisine: params[:cuisine])
+        r=Restaurant.all.find_by(cuisine: params[:cuisine])
+        r.to_json
     end
 
-    get '/restaurants' do
+    get '/restaurants/:id' do
         restaurant = Restaurant.find(params[:id])
         restaurant.to_json
     end
