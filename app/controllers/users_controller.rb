26f8d1c8229b83a8297393c154d87526ac637eb9
Marc Cavazos <marcavazos19@icloud.com>
2022-03-09 08:24:36 -0600

    backend updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < ApplicationController
+
     get '/users' do
         User.all.to_json
+    end
+
+    get '/user/reservation/:id' do
+        user = User.find(params[:id])
+        reservations = user.reservations.map do |r|
+            r
+        end
+        reservations.to_json(include: :restaurant)
+    end
+
+    post '/users/' do
+        user = User.create(
+            name: params[:name],
+            cc_info: params[:cc_info],
+            address: params[:address]
+        )
+
+        user.to_json
+    end
+
+    patch '/users/:id' do
+        user = User.find(params[:id])
+
+        attrs_to_update = params.select do |key, value|
+            ["name", "cc_info", "address"].include?(key)
+        end
+
+        user.update(attrs_to_update)
+
+        user.to_json
+    end
+
+    delete '/users' do
+        user = User.find(params[:id])
+
+        user.destroy
+
+        user.to_json
     end
 end
